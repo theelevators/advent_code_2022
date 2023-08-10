0753b83564a981119b7a232e3ad0a381d252de13
theelevators <elevatorstest@gmail.com>
2023-08-10 07:42:21 -0700

    day 3 solution 2 completed

--- advent_day_three.sql
+++ advent_day_three.sql
@@ -2,20 +2,15 @@
 
 IF OBJECT_ID('tempdb..#DayThree') IS NOT NULL DROP TABLE #DayThree;
 
-DECLARE @ParsedInput TABLE (LeftCompartment VARCHAR(100)
+DECLARE @ParsedInputOne TABLE (LeftCompartment VARCHAR(100)
 							,RightCompartment VARCHAR(100)
 							)
+DECLARE @ParsedInputTwo TABLE (GroupID INT
+							,Rucksack VARCHAR(100)
+							)
 
-SELECT REPLACE(RTRIM(LTRIM(value)), CHAR(10), '') rucksack
-INTO #DayThree
-FROM OPENROWSET(BULK N'D:\Projects\Rust\Advent\elves_way\rucksack.txt', SINGLE_CLOB) AS Contents
-CROSS APPLY string_split(BulkColumn, CHAR(13))
-
-INSERT INTO @ParsedInput
-SELECT LEFT(rucksack, LEN(rucksack)/2), RIGHT(rucksack, LEN(rucksack)/2)
-FROM #DayThree
-WHERE rucksack != ''
-
+DECLARE @GroupID INT = 0
+DECLARE @Sack VARCHAR(100)
 DECLARE @Left VARCHAR(100)
 DECLARE @Item CHAR(1)
 DECLARE @Right VARCHAR(100)
@@ -23,11 +18,25 @@ DECLARE @Items TABLE (Letter CHAR(1))
 DECLARE @Results TABLE (Letter CHAR(1)
 						,[Priority] INT)
 
+SELECT REPLACE(RTRIM(LTRIM(value)), CHAR(10), '') rucksack
+INTO #DayThree
+FROM OPENROWSET(BULK N'D:\Projects\Rust\Advent\elves_way\rucksack.txt', SINGLE_CLOB) AS Contents
+CROSS APPLY string_split(BulkColumn, CHAR(13))
 
-WHILE (SELECT TOP 1 LeftCompartment FROM @ParsedInput) IS NOT NULL
+
+DELETE FROM #DayThree
+WHERE rucksack = ''
+
+
+INSERT INTO @ParsedInputOne
+SELECT LEFT(rucksack, LEN(rucksack)/2), RIGHT(rucksack, LEN(rucksack)/2)
+FROM #DayThree
+
+
+WHILE (SELECT TOP 1 LeftCompartment FROM @ParsedInputOne) IS NOT NULL
 
 BEGIN
-	SELECT TOP 1 @Left = LeftCompartment, @Right = RightCompartment FROM @ParsedInput
+	SELECT TOP 1 @Left = LeftCompartment, @Right = RightCompartment FROM @ParsedInputOne
 	
 	WHILE LEN(@Left) > 0
 	BEGIN
@@ -53,7 +62,7 @@ BEGIN
 
 
 	DELETE FROM @Items
-	DELETE TOP (1) FROM @ParsedInput
+	DELETE TOP (1) FROM @ParsedInputOne
 
 END
 
@@ -62,6 +71,57 @@ SELECT
 	,SUM([Priority]) TotalSum
 FROM @Results
 
+DELETE FROM @Results
+DELETE FROM @Items
 
 
+WHILE (SELECT TOP 1 rucksack FROM #DayThree) IS NOT NULL
+BEGIN
+	
+	INSERT INTO @ParsedInputTwo
+	SELECT TOP 3 @GroupID, rucksack
+	FROM #DayThree
+
+	DELETE TOP (3) FROM #DayThree
+
+	SET @GroupID = @GroupID + 1
+
+END
+
+WHILE (SELECT TOP 1 rucksack FROM @ParsedInputTwo) IS NOT NULL
+BEGIN
+	SELECT TOP 1 @Sack = rucksack, @GroupID = GroupID FROM @ParsedInputTwo 
+	
+	WHILE LEN(@Sack) > 0
+	BEGIN
+		SET @Item = LEFT(@Sack, 1)
+
+		IF (SELECT COUNT(*) FROM @ParsedInputTwo WHERE GroupID = @GroupID AND Rucksack LIKE '%'+ @Item + '%' COLLATE SQL_Latin1_General_CP1_CS_AS)  = 3
+		BEGIN
+			INSERT INTO @Items
+			SELECT @Item
+		END
+		SET @Sack = RIGHT(@Sack, LEN(@Sack) - 1)
+	END
+
+	INSERT INTO @Results
+	SELECT 
+		DISTINCT 
+			Letter
+			,CASE	
+				WHEN Letter = LOWER(Letter) COLLATE SQL_Latin1_General_CP1_CS_AS THEN 1 + ASCII(Letter) - ASCII('a')
+				ELSE 27 + ASCII(Letter) - ASCII('A')
+			END [Priority]
+	FROM @Items
+
+	DELETE FROM @Items
+
+	DELETE TOP (3) FROM @ParsedInputTwo 
+
+END
+
+SELECT
+	'' SolutionTwo
+	,SUM([Priority]) TotalSum
+FROM @Results
 
